25ee34d8056f80bde5c04adc777f025ff3847eb7
bigbrotheriswatchu <kirill.maiboroda@mail.ru>
2020-04-06 10:43:40 +0300

    settings for images and secret keys

--- dating_app/models.py
+++ dating_app/models.py
@@ -7,13 +7,13 @@ from django.urls import reverse
 
 class UserProfile(models.Model):
     class Meta:
-        verbose_name        = 'Анкета'
+        verbose_name = 'Анкета'
         verbose_name_plural = 'Анкеты'
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     # choices for gender
-    MALE    = 'M'
-    FEMALE  = 'FM'
+    MALE = 'M'
+    FEMALE = 'FM'
     UNKNOWN = 'UNKW'
 
     GENDERS = (
@@ -24,24 +24,24 @@ class UserProfile(models.Model):
     genders = models.CharField(max_length=10, choices=GENDERS, default=UNKNOWN)
 
     # head information about profile
-    location    =   models.CharField(max_length=50)
-    age         =   models.PositiveIntegerField()
+    location = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
 
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(age__gte=18), name='age_gte_18'),
         ]
 
-    about_me    =   models.TextField(max_length=300)
-    avatar      =   ImageField(upload_to='profile_image', blank=True)
+    about_me = models.TextField(max_length=300)
+    avatar = ImageField(upload_to='profile_image', blank=True)
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
 
 
 class Interests(models.Model):
-    interests   =   models.ManyToManyField(UserProfile, blank=True)
-    title       =   models.CharField(max_length=50, blank=True)
+    interests = models.ManyToManyField(UserProfile, blank=True)
+    title = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.title
@@ -49,12 +49,12 @@ class Interests(models.Model):
 
 class Dialog(models.Model):
     class Meta:
-        verbose_name        = 'Диалог'
+        verbose_name = 'Диалог'
         verbose_name_plural = 'Диалоги'
 
-    owner       =   models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Dialog owner", related_name="selfDialogs",
-                                on_delete=models.CASCADE)
-    opponent    =   models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Dialog opponent", on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Dialog owner", related_name="selfDialogs",
+                              on_delete=models.CASCADE)
+    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Dialog opponent", on_delete=models.CASCADE)
 
     def __str__(self):
         return _("Chat with ") + self.opponent.username
@@ -62,19 +62,19 @@ class Dialog(models.Model):
 
 class Message(models.Model):
     class Meta:
-        verbose_name        = 'Сообщение'
+        verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
 
-    dialog      =   models.ForeignKey(Dialog, verbose_name="Dialog", related_name="messages", on_delete=models.CASCADE)
-    sender      =   models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Author", on_delete=models.CASCADE)
-    text        =   models.TextField(verbose_name="Message text")
-    read        =   models.BooleanField(verbose_name="Read", default=False)
-    pub_date    =   models.DateTimeField(auto_now_add=True)
+    dialog = models.ForeignKey(Dialog, verbose_name="Dialog", related_name="messages", on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Author", on_delete=models.CASCADE)
+    text = models.TextField(verbose_name="Message text")
+    read = models.BooleanField(verbose_name="Read", default=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
 
 class MatchFriend(models.Model):
-    users       =   models.ManyToManyField(UserProfile)
-    current_user    =   models.ForeignKey(UserProfile, related_name='owner',  on_delete=models.CASCADE, default=False)
+    users = models.ManyToManyField(UserProfile)
+    current_user = models.ForeignKey(UserProfile, related_name='owner', on_delete=models.CASCADE, default=False)
 
     @classmethod
     def make_friend(cls, current_user, new_match_friend):
@@ -89,4 +89,3 @@ class MatchFriend(models.Model):
             current_user=current_user
         )
         friend.users.remove(new_match_friend)
-
--- dating_online/settings.py
+++ dating_online/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0eki3$v2nt0yqhecqv5+xi%awy(1@+66&xl#u!=jemhc^zigxe'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,10 +118,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-# setting for static files in developing and prod
+# setting for dec
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
 
+
+# setting for static files in developing and prod
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
@@ -134,8 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # S3 BUCKETS CONFIG
-AWS_ACCESS_KEY_ID = 'AKIA6LDVGNCLUJD3QBJB'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'online-dating-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
